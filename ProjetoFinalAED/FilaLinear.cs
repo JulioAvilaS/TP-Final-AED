97b0c8a9c71d1b1db3792c0ba15f9b3484c2e9bf
Kelvin Lemos dos Santos <1526372@sga.pucminas.br>
2024-12-05 11:40:55 -0300

    Fiz a fila, estou revisando

--- ProjetoFinalAED/FilaLinear.cs
+++ ProjetoFinalAED/FilaLinear.cs
@@ -8,6 +8,19 @@ namespace ProjetoFinalAED
 {
     internal class FilaLinear
     {
-
+        private Candidato[] array;
+        int primeiro, ultimo;
+        public FilaLinear(int tamanho)
+        {
+            array = new Candidato[tamanho + 1];
+            primeiro = ultimo = 0;
+        }
+        public void Inserir(Candidato x)
+        {
+            if (((ultimo + 1) % array.Length) == primeiro)
+                throw new Exception("Erro!");
+            array[ultimo] = x;
+            ultimo = (ultimo + 1) % array.Length;
+        }
     }
 }
